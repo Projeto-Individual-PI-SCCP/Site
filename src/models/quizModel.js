ba09e035f5ae7197f451502be9ae09fa049d9836
Ornaghiii <guilherme.ornaghi@sptech.school>
2025-11-22 10:58:32 -0300

    Mudanças na Dashboard e outras páginas

--- src/models/quizModel.js
+++ src/models/quizModel.js
@@ -4,7 +4,7 @@ function registrarTentativa(idQuiz, idUsuario, acertos, erros, pontuacao) {
 
     var instrucaoSql = `
         INSERT INTO QuizPontuacao (Quiz_idQuiz, Usuario_idUsuario, pontuacao, acertos, erros)
-        VALUES (${idQuiz},${idUsuario}, ${pontuacao}, ${acertos}, ${erros});
+        VALUES (${idQuiz}, ${idUsuario}, ${pontuacao}, ${acertos}, ${erros});
     `;
 
     console.log("Executando a instrução SQL:\n" + instrucaoSql);
